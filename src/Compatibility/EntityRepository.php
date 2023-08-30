c0c0663b3d81940d9b5c5eb87e6fa4766ef17cf4
Uwe Kleinmann <uwe@kleinmann.org>
2023-08-30 12:27:59 +0200

    chore: Remove update branch from GitHub Workflow, remove unneeded gitignore files, add compatibility TODO notice.

--- src/Compatibility/EntityRepository.php
+++ src/Compatibility/EntityRepository.php
@@ -6,6 +6,7 @@ namespace Shopware\Core\Framework\DataAbstractionLayer;
 
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 
+// TODO: Remove me if compatibility is at least 6.5.0.0
 if (!class_exists('Shopware\Core\Framework\DataAbstractionLayer\EntityRepository')) {
     class EntityRepository implements EntityRepositoryInterface
     {
