bdc5d9715ffc63fa1390da849ea3795bfa234186
pawel.karczmarczyk <pawel.karczmarczyk@external.nexigroup.com>
2023-12-01 08:55:44 +0000

    Merged PR 32000: Payment process exceptions are caught without further action in \Nets\Checkout\Service\Checkout . T
    Exceptions added and split
    
    Related work items: #78164

--- src/Service/Checkout.php
+++ src/Service/Checkout.php
@@ -7,8 +7,6 @@ namespace Nets\Checkout\Service;
 use Nets\Checkout\Facade\AsyncPaymentFinalizePay;
 use Shopware\Core\Checkout\Payment\Cart\AsyncPaymentTransactionStruct;
 use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\AsynchronousPaymentHandlerInterface;
-use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentFinalizeException;
-use Shopware\Core\Checkout\Payment\Exception\CustomerCanceledAsyncPaymentException;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -25,19 +23,13 @@ class Checkout implements AsynchronousPaymentHandlerInterface
 
     public function pay(AsyncPaymentTransactionStruct $transaction, RequestDataBag $dataBag, SalesChannelContext $salesChannelContext): RedirectResponse
     {
-        try {
             $paymentUrl = $this->payFinalizeFacade->pay($transaction, $dataBag, $salesChannelContext);
 
             return new RedirectResponse((string) $paymentUrl);
-        } catch (AsyncPaymentFinalizeException|CustomerCanceledAsyncPaymentException $ex) {
-        }
     }
 
     public function finalize(AsyncPaymentTransactionStruct $transaction, Request $request, SalesChannelContext $salesChannelContext): void
     {
-        try {
             $this->payFinalizeFacade->finalize($transaction, $request, $salesChannelContext);
-        } catch (AsyncPaymentFinalizeException|CustomerCanceledAsyncPaymentException $ex) {
-        }
     }
 }
