d6e0711a6a02f5dfd77c93563a9f9d24ce01d0e3
l.brodowski <lukasz.brodowski@external.nexigroup.com>
2023-12-13 08:45:08 +0100

    Prettier formatting

--- src/Resources/app/administration/src/mixin/nets-checkout-order.mixin.js
+++ src/Resources/app/administration/src/mixin/nets-checkout-order.mixin.js
@@ -1,125 +1,148 @@
 const { Mixin } = Shopware;
 
-Mixin.register('nets-checkout-order', {
-    methods: {
-        getTransactionId(transaction) {
-            let result = null;
-            if(transaction.hasOwnProperty('customFields') && transaction['customFields']) {
-                if(transaction.customFields.hasOwnProperty('nets_easy_payment_details') &&
-                    transaction.customFields['nets_easy_payment_details']) {
-                    result = transaction.customFields.nets_easy_payment_details.transaction_id;
-                }
-            }
-            return result;
-        },
+Mixin.register("nets-checkout-order", {
+  methods: {
+    getTransactionId(transaction) {
+      let result = null;
+      if (
+        transaction.hasOwnProperty("customFields") &&
+        transaction["customFields"]
+      ) {
+        if (
+          transaction.customFields.hasOwnProperty(
+            "nets_easy_payment_details",
+          ) &&
+          transaction.customFields["nets_easy_payment_details"]
+        ) {
+          result =
+            transaction.customFields.nets_easy_payment_details.transaction_id;
+        }
+      }
+      return result;
+    },
 
-        // canCapture(orderStateTechnicalName) {
-        //     const isCapturingEnabled = this.amountAvailableForCapturing > 0;
-        //
-        //     if(this.amountAvailableForCapturing > 0 && orderStateTechnicalName != "cancelled") {
-        //         return true;
-        //     }
-        //     return false;
-        // },
+    // canCapture(orderStateTechnicalName) {
+    //     const isCapturingEnabled = this.amountAvailableForCapturing > 0;
+    //
+    //     if(this.amountAvailableForCapturing > 0 && orderStateTechnicalName != "cancelled") {
+    //         return true;
+    //     }
+    //     return false;
+    // },
 
-        canCapture(orderStateTechnicalName) {
-            return orderStateTechnicalName === "authorized" || orderStateTechnicalName === "paid_partially";
-        },
+    canCapture(orderStateTechnicalName) {
+      return (
+        orderStateTechnicalName === "authorized" ||
+        orderStateTechnicalName === "paid_partially"
+      );
+    },
 
+    getSummaryAmounts(order) {
+      let me;
+      me = this;
+      me.isLoading = true;
 
-        getSummaryAmounts(order) {
-            let me;
-            me = this;
-            me.isLoading = true;
+      if (this.getTransactionId(order.transactions.first())) {
+        this.NetsCheckoutApiPaymentService.getSummaryAmounts(order)
+          .then((response) => {
+            //
+            me.amountAvailableForCapturing =
+              response.amountAvailableForCapturing;
+            me.amountAvailableForRefunding =
+              response.amountAvailableForRefunding;
+            me.isLoading = false;
+            me.orderState = response.orderState;
+            me.refundPendingStatus = response.refundPendingStatus;
+            me.paymentMethod = response.paymentMethod;
+          })
+          .catch((errorResponse) => {
+            //
+            me.isLoading = false;
+          });
+      } else {
+        me.isLoading = false;
+      }
+    },
 
-            if(this.getTransactionId(order.transactions.first())) {
-                this.NetsCheckoutApiPaymentService.getSummaryAmounts(order)
-                    .then((response) => {
-                        //
-                        me.amountAvailableForCapturing = response.amountAvailableForCapturing;
-                        me.amountAvailableForRefunding = response.amountAvailableForRefunding;
-                        me.isLoading = false;
-                        me.orderState = response.orderState;
-                        me.refundPendingStatus = response.refundPendingStatus;
-                        me.paymentMethod = response.paymentMethod;
-                    })
-                    .catch((errorResponse) => {
-                        //
-                        me.isLoading = false;
-                    });
-            } else {
-                me.isLoading = false;
-            }
-        },
+    // canRefund(orderStateTechnicalName) {
+    //     if(this.refundPendingStatus){
+    //         return false;
+    //     }
+    //     if(this.amountAvailableForRefunding > 0 && this.amountAvailableForCapturing == 0 && orderStateTechnicalName != "cancelled") {
+    //         return true;
+    //     }
+    //
+    //     return false;
+    // },
 
-        // canRefund(orderStateTechnicalName) {
-        //     if(this.refundPendingStatus){
-        //         return false;
-        //     }
-        //     if(this.amountAvailableForRefunding > 0 && this.amountAvailableForCapturing == 0 && orderStateTechnicalName != "cancelled") {
-        //         return true;
-        //     }
-        //
-        //     return false;
-        // },
+    canRefund(orderStateTechnicalName) {
+      console.log(orderStateTechnicalName);
+      if (this.refundPendingStatus) {
+        return false;
+      }
+      return (
+        orderStateTechnicalName === "paid" ||
+        orderStateTechnicalName === "paid_partially"
+      );
+    },
 
-        canRefund(orderStateTechnicalName) {
-            console.log(orderStateTechnicalName)
-            if(this.refundPendingStatus){
-                return false;
-            }
-            return orderStateTechnicalName === "paid" || orderStateTechnicalName === "paid_partially";
-        },
+    capture(paymentId, order) {
+      let me = this;
+      const orderId = order.id;
+      const amount = this.amountAvailableForCapturing;
+      me.isLoading = true;
 
-        capture(paymentId, order) {
-            let me = this;
-            const orderId = order.id;
-            const amount = this.amountAvailableForCapturing;
-            me.isLoading = true;
+      this.NetsCheckoutApiPaymentService.captureTransaction(
+        orderId,
+        paymentId,
+        amount,
+      )
+        .then((result) => {
+          this.createNotificationSuccess({
+            title: this.$tc("Nets"),
+            message: this.$tc("Capture processed successfully."),
+          });
+          me.isLoading = false;
+          this.getSummaryAmounts(order);
+        })
+        .catch((errorResponse) => {
+          this.createNotificationError({
+            title: this.$tc("Nets"),
+            message: this.$tc(errorResponse.message),
+          });
+          me.isLoading = false;
+          this.getSummaryAmounts(order);
+        });
+    },
 
-            this.NetsCheckoutApiPaymentService.captureTransaction(orderId, paymentId, amount)
-                .then((result) => {
-                    this.createNotificationSuccess({
-                        title: this.$tc('Nets'),
-                        message: this.$tc('Capture processed successfully.')
-                    });
-                    me.isLoading = false;
-                    this.getSummaryAmounts(order);
-                })
-                .catch((errorResponse) => {
-                    this.createNotificationError({
-                        title: this.$tc('Nets'),
-                        message: this.$tc(errorResponse.message)
-                    });
-                    me.isLoading = false;
-                    this.getSummaryAmounts(order);
-                });
-        },
+    refund(paymentId, order) {
+      let me = this;
+      me.isLoading = true;
 
-        refund(paymentId, order) {
-            let me = this;
-            me.isLoading = true;
+      const orderId = order.id;
+      const amount = this.amountAvailableForRefunding;
 
-            const orderId = order.id;
-            const amount = this.amountAvailableForRefunding;
-
-            this.NetsCheckoutApiPaymentService.refundTransaction(orderId, paymentId, amount)
-                .then(() => {
-                    this.createNotificationSuccess({
-                        title: this.$tc('Nets'),
-                        message: this.$tc('Refund processed successfully.')
-                    });
-                    me.isLoading = false;
-                    this.getSummaryAmounts(order);
-                })
-                .catch((errorResponse) => {
-                    this.createNotificationError({
-                        title: this.$tc('Nets'),
-                        message: this.$tc('Error occurred during refund')
-                    });
-                    me.isLoading = false;
-                    this.getSummaryAmounts(order);
-                });
-        },
-    }
+      this.NetsCheckoutApiPaymentService.refundTransaction(
+        orderId,
+        paymentId,
+        amount,
+      )
+        .then(() => {
+          this.createNotificationSuccess({
+            title: this.$tc("Nets"),
+            message: this.$tc("Refund processed successfully."),
+          });
+          me.isLoading = false;
+          this.getSummaryAmounts(order);
+        })
+        .catch((errorResponse) => {
+          this.createNotificationError({
+            title: this.$tc("Nets"),
+            message: this.$tc("Error occurred during refund"),
+          });
+          me.isLoading = false;
+          this.getSummaryAmounts(order);
+        });
+    },
+  },
 });
