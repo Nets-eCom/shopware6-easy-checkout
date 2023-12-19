2ea57ba86169a5e8c40acf1feb5d3af7bfca52dc
l.brodowski <lukasz.brodowski@external.nexigroup.com>
2023-12-19 16:33:58 +0100

    fix(embedded): redirect to finalize endpoint

--- src/Controller/Storefront/PaymentController.php
+++ src/Controller/Storefront/PaymentController.php
@@ -8,23 +8,19 @@ use Nets\Checkout\Service\Easy\Api\EasyApiService;
 use Nets\Checkout\Service\Easy\Api\Exception\EasyApiException;
 use Shopware\Core\Checkout\Cart\SalesChannel\AbstractCartOrderRoute;
 use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
-use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionDefinition;
-use Shopware\Core\Checkout\Payment\PaymentService;
+use Shopware\Core\Checkout\Payment\Cart\Token\TokenFactoryInterfaceV2;
+use Shopware\Core\Checkout\Payment\Cart\Token\TokenStruct;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Core\System\StateMachine\Aggregation\StateMachineTransition\StateMachineTransitionActions;
-use Shopware\Core\System\StateMachine\StateMachineRegistry;
-use Shopware\Core\System\StateMachine\Transition;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 use Nets\Checkout\Service\DataReader\OrderDataReader;
-use Nets\Checkout\Service\Easy\ConfigService;
 
 /**
  * @RouteScope(scopes={"storefront"})
@@ -33,38 +29,29 @@ use Nets\Checkout\Service\Easy\ConfigService;
 class PaymentController extends StorefrontController
 {
     private EasyApiService $easyApiService;
-    private ConfigService $configService;
     private CartService $cartService;
-    private PaymentService $paymentService;
-    private EntityRepository $netsApiRepository;
-    private StateMachineRegistry $stateMachineRegistry;
     private AbstractCartOrderRoute $orderRoute;
     private RequestStack $requestStack;
     private EntityRepository $orderTransactionRepo;
     private OrderDataReader $orderDataReader;
+    private TokenFactoryInterfaceV2 $tokenFactory;
 
     public function __construct(
         EasyApiService $easyApiService,
-        ConfigService $configService,
         CartService $cartService,
-        PaymentService $paymentService,
-        EntityRepository $netsApiRepository,
-        StateMachineRegistry $machineRegistry,
         AbstractCartOrderRoute $orderRoute,
         RequestStack $requestStack,
         EntityRepository $orderTransactionRepo,
-        OrderDataReader $orderDataReader
+        OrderDataReader $orderDataReader,
+        TokenFactoryInterfaceV2 $tokenFactory
     ) {
         $this->easyApiService       = $easyApiService;
-        $this->configService        = $configService;
         $this->cartService          = $cartService;
-        $this->paymentService       = $paymentService;
-        $this->netsApiRepository    = $netsApiRepository;
-        $this->stateMachineRegistry = $machineRegistry;
         $this->orderRoute           = $orderRoute;
         $this->requestStack         = $requestStack;
         $this->orderTransactionRepo = $orderTransactionRepo;
         $this->orderDataReader      = $orderDataReader;
+        $this->tokenFactory = $tokenFactory;
     }
 
     /**
@@ -88,33 +75,11 @@ class PaymentController extends StorefrontController
         $orderEntity    = $this->orderDataReader->getOrderEntityById($context, $orderId);
         $transaction    = $orderEntity->getTransactions()->first();
         $payment     = $this->easyApiService->getPayment($this->requestStack->getCurrentRequest()->get('paymentId'));
-        $checkoutUrl = $payment->getCheckoutUrl();
-        $refUpdate   = [
+
+        $this->easyApiService->updateReference($this->requestStack->getCurrentRequest()->get('paymentId'), json_encode([
             'reference'   => $orderEntity->getOrderNumber(),
-            'checkoutUrl' => $checkoutUrl,
-        ];
-        $this->easyApiService->updateReference($this->requestStack->getCurrentRequest()->get('paymentId'), json_encode($refUpdate));
-
-        // For inserting amount available respect to charge id
-        if ($this->configService->getChargeNow($ctx->getsalesChannel()->getId()) == 'yes' || $payment->getPaymentType() == 'A2A') {
-            $this->netsApiRepository->create([
-            [
-            'order_id'         => $orderId ? $orderId : '',
-            'charge_id'        => $payment->getFirstChargeId() ? $payment->getFirstChargeId() : '',
-            'operation_type'   => 'capture',
-            'operation_amount' => $payment->getChargedAmount() ? $payment->getChargedAmount() / 100 : '',
-            'amount_available' => $payment->getChargedAmount() ? $payment->getChargedAmount() / 100 : '',
-            ],
-            ], $context);
-
-            $this->stateMachineRegistry->transition(new Transition(
-                OrderTransactionDefinition::ENTITY_NAME,
-                $orderEntity->getTransactions()->first()
-                ->getId(),
-                StateMachineTransitionActions::ACTION_PAID,
-                'stateId'
-            ), $context);
-        }
+            'checkoutUrl' => $payment->getCheckoutUrl(),
+        ]));
 
         $this->orderTransactionRepo->update([
                [
@@ -126,18 +91,26 @@ class PaymentController extends StorefrontController
                        ],
                    ],
                ],
-           ], $context);
+           ],
+            $context
+        );
 
-        $finishUrl = $this->generateUrl('frontend.checkout.finish.page',
+        $tokenStruct = new TokenStruct(
+            null,
+            null,
+            $transaction->getPaymentMethodId(),
+            $transaction->getId(),
+            $this->generateUrl(
+                'frontend.checkout.finish.page', ['orderId' => $orderId]
+            ),
+        );
+
+        return $this->redirectToRoute(
+            'payment.finalize.transaction',
             [
-                'orderId' => $orderId,
-            ]);
-
-        try {
-            $this->paymentService->handlePaymentByOrder($orderId, $data, $ctx, $finishUrl);
-
-            return new RedirectResponse($finishUrl);
-        } catch (\Exception $e) {
-        }
+                '_sw_payment_token' => $this->tokenFactory->generateToken($tokenStruct),
+                "paymentid" => $this->requestStack->getCurrentRequest()->get('paymentId')
+            ]
+        );
     }
 }
