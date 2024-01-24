12b4ee00d80fe6beffa8e5f4c17fd25461e6c517
wellfix <dbrozyna15@gmail.com>
2024-01-24 16:50:52 +0100

    feat: added phpstan ignore comments to rector config and changed non-abstract class EntityRepository.php to abstract

--- rector.php
+++ rector.php
@@ -6,8 +6,9 @@ use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
 
+/** @phpstan-ignore-next-line  */
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->phpVersion(PhpVersion::PHP_74); /** @phpstan-ignore-line  */
     $rectorConfig->paths([
         __DIR__ . '/src',
     ]);
@@ -17,10 +18,10 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::DEAD_CODE,
-        SetList::STRICT_BOOLEANS,
+        SetList::CODE_QUALITY, /** @phpstan-ignore-line  */
+        SetList::CODING_STYLE, /** @phpstan-ignore-line  */
+        SetList::DEAD_CODE, /** @phpstan-ignore-line  */
+        SetList::STRICT_BOOLEANS, /** @phpstan-ignore-line  */
     ]);
 
     $rectorConfig->importNames();
