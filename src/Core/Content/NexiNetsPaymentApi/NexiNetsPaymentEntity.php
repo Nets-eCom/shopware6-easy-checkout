9b36a8e9089bf09023973170f20794d07c4fd500
l.brodowski <lukasz.brodowski@external.nexigroup.com>
2024-06-25 12:47:24 +0200

    Create and register entity classes

---
+++ src/Core/Content/NexiNetsPaymentApi/NexiNetsPaymentEntity.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NexiNets\Core\Content\NexiNetsPaymentApi;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+
+class NexiNetsPaymentEntity extends Entity
+{
+    use EntityIdTrait;
+
+    protected ?string $name;
+
+    protected ?string $description;
+
+    protected bool $active;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): void
+    {
+        $this->active = $active;
+    }
+}
