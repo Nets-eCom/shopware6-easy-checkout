30cee632aec60926a55c74435f463e9a10658738
l.brodowski <lukasz.brodowski@external.nexigroup.com>
2024-06-27 12:01:32 +0200

    fix: resolving phpstan error

--- src/Entity/NexiNetsPaymentApi/NexiNetsPaymentCollection.php
+++ src/Entity/NexiNetsPaymentApi/NexiNetsPaymentCollection.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace NexiNets\Entity\NexiNetsPaymentApi;
 
-use NexiNets\Core\Content\NexiNetsPaymentApi\ExampleEntity;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 
 /**
- * @method void               add(ExampleEntity $entity)
- * @method void               set(string $key, ExampleEntity $entity)
- * @method ExampleEntity[]    getIterator()
- * @method ExampleEntity[]    getElements()
- * @method ExampleEntity|null get(string $key)
- * @method ExampleEntity|null first()
- * @method ExampleEntity|null last()
+ * @extends EntityCollection<NexiNetsPaymentEntity>
+ *
+ * @method void               add(NexiNetsPaymentEntity $entity)
+ * @method void               set(string $key, NexiNetsPaymentEntity $entity)
+ * @method NexiNetsPaymentEntity[]    getIterator()
+ * @method NexiNetsPaymentEntity[]    getElements()
+ * @method NexiNetsPaymentEntity|null get(string $key)
+ * @method NexiNetsPaymentEntity|null first()
+ * @method NexiNetsPaymentEntity|null last()
  */
 class NexiNetsPaymentCollection extends EntityCollection
 {
