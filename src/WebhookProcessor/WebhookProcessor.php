514bfde03d729858285d4cef43e57b7a68f413ad
Slawomir Rostek <slawomir.rostek@external.nexigroup.com>
2024-08-05 14:30:47 +0200

    feat: #96548 added webhook controller

---
+++ src/WebhookProcessor/WebhookProcessor.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace NexiNets\WebhookProcessor;
+
+use NexiNets\CheckoutApi\Model\Result\Webhook\Webhook;
+
+final class WebhookProcessor
+{
+    /**
+     * @var WebhookInterface[]
+     */
+    private iterable $webhookProcessors = [];
+
+    public function __construct(
+        WebhookInterface ...$webhookProcessors
+    ) {
+        $this->webhookProcessors = $webhookProcessors;
+    }
+
+    /**
+     * @throws WebhookProcessorException
+     * @throws \RuntimeException
+     */
+    public function process(Webhook $webhook): void
+    {
+        /** @var WebhookInterface $webhookProcessor */
+        foreach ($this->webhookProcessors as $webhookProcessor) {
+            if ($webhook->getEvent() === $webhookProcessor->getEvent()) {
+                $webhookProcessor->process($webhook);
+            }
+        }
+
+        throw new \RuntimeException('Invalid webhook event');
+    }
+}
