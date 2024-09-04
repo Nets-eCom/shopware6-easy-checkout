7ce0b5c2f09ab96e0ceb75b00f95d0a6909fc216
Slawomir Rostek <slawomir.rostek@external.nexigroup.com>
2024-09-04 13:41:32 +0200

    feat: added sw-flow-sequence-action js files

---
+++ src/Resources/app/administration/src/constant/nexinets-charge-action.constant.js
@@ -0,0 +1,9 @@
+export const ACTION = Object.freeze({
+    NEXINETS_CHARGE: 'action.nexinets.charge',
+});
+
+export const GROUP = 'order'
+
+export default {
+    ACTION, GROUP
+};
---
+++ src/Resources/app/administration/src/main.js
@@ -0,0 +1,1 @@
+import './extension/sw-flow-sequence-action';
