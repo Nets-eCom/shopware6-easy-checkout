cfb7ee67af41816fa519ac601f3f78d6feb9b342
Slawomir Rostek <slawomir.rostek@external.nexigroup.com>
2024-10-16 14:01:44 +0200

    feat: #100193 payment fetcher test

---
+++ tests/Fetcher/PaymentFetcherTest.php
@@ -0,0 +1,92 @@
+<?php declare(strict_types=1);
+
+namespace NexiNets\Tests\Fetcher;
+
+use NexiNets\CheckoutApi\Api\PaymentApi;
+use NexiNets\CheckoutApi\Factory\PaymentApiFactory;
+use NexiNets\CheckoutApi\Model\Result\RetrievePaymentResult;
+use NexiNets\Configuration\ConfigurationProvider;
+use NexiNets\Fetcher\PaymentFetcher;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class PaymentFetcherTest extends TestCase
+{
+    private PaymentApiFactory|MockObject $paymentApiFactory;
+
+    private ConfigurationProvider|MockObject $configurationProvider;
+
+    private PaymentApi|MockObject $paymentApi;
+
+    protected function setUp(): void
+    {
+        $this->paymentApiFactory = $this->createMock(PaymentApiFactory::class);
+        $this->configurationProvider = $this->createMock(ConfigurationProvider::class);
+        $this->paymentApi = $this->createMock(PaymentApi::class);
+    }
+
+    public function testFetchPayment(): void
+    {
+        $salesChannelId = 'test_sales_channel';
+        $paymentId = 'test_payment_id';
+
+        $this->configurationProvider
+            ->method('getSecretKey')
+            ->with($salesChannelId)
+            ->willReturn('secret_key');
+
+        $this->configurationProvider
+            ->method('isLiveMode')
+            ->with($salesChannelId)
+            ->willReturn(true);
+
+        $this->paymentApiFactory
+            ->method('create')
+            ->with('secret_key', true)
+            ->willReturn($this->paymentApi);
+
+        $paymentResult = $this->createPaymentResult();
+        $this->paymentApi
+            ->method('retrievePayment')
+            ->with($paymentId)
+            ->willReturn($paymentResult);
+
+        $sut = new PaymentFetcher(
+            $this->paymentApiFactory,
+            $this->configurationProvider
+        );
+        $result = $sut->fetchPayment($salesChannelId, $paymentId);
+
+        $this->assertSame($paymentResult->getPayment(), $result);
+    }
+
+    private function createPaymentResult(): RetrievePaymentResult
+    {
+        return RetrievePaymentResult::fromJson('{
+            "payment": {
+                "paymentId": "025400006091b1ef6937598058c4e487",
+                "summary": {
+                    "reservedAmount": 100
+                },
+                "consumer": {
+                    "shippingAddress": {},
+                    "billingAddress": {},
+                    "privatePerson": {},
+                    "company": {}
+                },
+                "paymentDetails": {},
+                "orderDetails": {
+                    "amount": 100,
+                    "currency": "EUR"
+                },
+                "checkout": {
+                    "url": "https://example.com/checkout",
+                    "cancelUrl": null
+                },
+                "created": "2019-08-24T14:15:22Z",
+                "refunds": [],
+                "charges": []
+            }
+        }');
+    }
+}
