6eff0e699fbc416761d291be926415cb19921fc4
piotr.karszny <piotr.karszny@external.nexigroup.com>
2024-10-29 14:27:41 +0100

    feat: introduce charge endpoint
    Refs: #98831

--- src/Core/Content/Flow/Dispatching/Action/ChargeAction.php
+++ src/Core/Content/Flow/Dispatching/Action/ChargeAction.php
@@ -1,15 +1,10 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace NexiNets\Core\Content\Flow\Dispatching\Action;
 
-use NexiNets\CheckoutApi\Api\PaymentApi;
-use NexiNets\CheckoutApi\Factory\PaymentApiFactory;
-use NexiNets\CheckoutApi\Model\Result\RetrievePayment\PaymentStatusEnum;
-use NexiNets\Configuration\ConfigurationProvider;
-use NexiNets\Dictionary\OrderTransactionDictionary;
-use NexiNets\RequestBuilder\ChargeRequest;
-use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
-use Shopware\Core\Checkout\Order\OrderEntity;
+use NexiNets\Order\OrderCharge;
 use Shopware\Core\Content\Flow\Dispatching\Action\FlowAction;
 use Shopware\Core\Content\Flow\Dispatching\StorableFlow;
 use Shopware\Core\Framework\Event\OrderAware;
@@ -17,9 +12,7 @@ use Shopware\Core\Framework\Event\OrderAware;
 class ChargeAction extends FlowAction
 {
     public function __construct(
-        private readonly ConfigurationProvider $configurationProvider,
-        private readonly ChargeRequest $chargeRequest,
-        private readonly PaymentApiFactory $paymentApiFactory,
+        private readonly OrderCharge $orderCharge
     ) {
     }
 
@@ -39,45 +32,6 @@ class ChargeAction extends FlowAction
             return;
         }
 
-        /** @var OrderEntity $order */
-        $order = $flow->getData(OrderAware::ORDER);
-
-        if ($this->configurationProvider->isAutoCharge($order->getSalesChannelId())) {
-            return;
-        }
-
-        $transactions = $order->getTransactions();
-
-        if ($transactions === null) {
-            return;
-        }
-
-        /** @var OrderTransactionEntity $transaction */
-        foreach ($transactions as $transaction) {
-            $paymentId = $transaction->getCustomFieldsValue(
-                OrderTransactionDictionary::CUSTOM_FIELDS_NEXI_NETS_PAYMENT_ID
-            );
-
-            if ($paymentId === null) {
-                continue;
-            }
-
-            $paymentApi = $this->createPaymentApi($order->getSalesChannelId());
-            $payment = $paymentApi->retrievePayment($paymentId)->getPayment();
-            if ($payment->getStatus() !== PaymentStatusEnum::RESERVED) { // we only allow to charge full amount
-                continue;
-            }
-
-            $charge = $this->chargeRequest->buildFullCharge($transaction);
-            $paymentApi->charge($paymentId, $charge);
-        }
-    }
-
-    private function createPaymentApi(string $salesChannelId): PaymentApi
-    {
-        return $this->paymentApiFactory->create(
-            $this->configurationProvider->getSecretKey($salesChannelId),
-            $this->configurationProvider->isLiveMode($salesChannelId),
-        );
+        $this->orderCharge->fullCharge($flow->getData(OrderAware::ORDER));
     }
 }
---
+++ tests/Order/OrderChargeTest.php
@@ -0,0 +1,265 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NexiNets\Tests\Order;
+
+use NexiNets\CheckoutApi\Api\PaymentApi;
+use NexiNets\CheckoutApi\Factory\PaymentApiFactory;
+use NexiNets\CheckoutApi\Model\Request\FullCharge;
+use NexiNets\CheckoutApi\Model\Result\ChargeResult;
+use NexiNets\CheckoutApi\Model\Result\RetrievePaymentResult;
+use NexiNets\Configuration\ConfigurationProvider;
+use NexiNets\Dictionary\OrderTransactionDictionary;
+use NexiNets\Order\OrderCharge;
+use NexiNets\RequestBuilder\ChargeRequest;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionCollection;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
+use Shopware\Core\Checkout\Order\OrderEntity;
+
+final class OrderChargeTest extends TestCase
+{
+    public function testItFullChargesOrder(): void
+    {
+        $order = $this->createOrderEntity();
+        $transaction = new OrderTransactionEntity();
+        $transaction->setId('transaction_uuid');
+        $transaction->setCustomFields([
+            OrderTransactionDictionary::CUSTOM_FIELDS_NEXI_NETS_PAYMENT_ID => '025400006091b1ef6937598058c4e487',
+        ]);
+        $order->getTransactions()->add($transaction);
+
+        $configurationProvider = $this->createConfigurationProviderMock();
+        $configurationProvider
+            ->method('isAutoCharge')
+            ->with('test_sales_channel_id')
+            ->willReturn(false);
+
+        $paymentApi = $this->createMock(PaymentApi::class);
+        $paymentApi->expects($this->once())
+            ->method('retrievePayment')
+            ->with('025400006091b1ef6937598058c4e487')
+            ->willReturn($this->createNotChargedRetrievePaymentResult());
+        $paymentApiFactory = $this->createPaymentApiFactoryMock($paymentApi);
+
+        $charge = new FullCharge(100);
+        $chargeRequestBuilder = $this->createMock(ChargeRequest::class);
+        $chargeRequestBuilder->expects($this->once())
+            ->method('buildFullCharge')
+            ->with($transaction)
+            ->willReturn($charge);
+
+        $paymentApi->expects($this->once())
+            ->method('charge')
+            ->with('025400006091b1ef6937598058c4e487', $charge)
+            ->willReturn(new ChargeResult('test_charge_id'));
+
+        $sut = new OrderCharge(
+            $paymentApiFactory,
+            $configurationProvider,
+            $chargeRequestBuilder,
+        );
+
+        $sut->fullCharge(
+            $order
+        );
+    }
+
+    public function testShouldNotFullChargeOnAutoChargeEnabled(): void
+    {
+        $order = $this->createOrderEntity();
+        $configurationProvider = $this->createConfigurationProviderMock();
+        $configurationProvider
+            ->method('isAutoCharge')
+            ->with('test_sales_channel_id')
+            ->willReturn(false);
+
+        $paymentApi = $this->createMock(PaymentApi::class);
+        $paymentApiFactory = $this->createPaymentApiFactoryMock($paymentApi);
+
+        $chargeRequestBuilder = $this->createMock(ChargeRequest::class);
+
+        $paymentApi->expects($this->never())->method('charge');
+
+        $sut = new OrderCharge(
+            $paymentApiFactory,
+            $configurationProvider,
+            $chargeRequestBuilder,
+        );
+
+        $sut->fullCharge($order);
+    }
+
+    public function testItShouldNotChargeIfNotNexiPayment(): void
+    {
+        $order = $this->createOrderEntity();
+        $transaction = new OrderTransactionEntity();
+        $transaction->setId('transaction_uuid');
+        $order->getTransactions()->add($transaction);
+
+        $configurationProvider = $this->createConfigurationProviderMock();
+        $configurationProvider
+            ->method('isAutoCharge')
+            ->with('test_sales_channel_id')
+            ->willReturn(false);
+
+        $paymentApi = $this->createMock(PaymentApi::class);
+        $paymentApi->expects($this->never())
+            ->method('retrievePayment');
+        $paymentApiFactory = $this->createPaymentApiFactoryMock($paymentApi);
+
+        $chargeRequestBuilder = $this->createMock(ChargeRequest::class);
+
+        $paymentApi->expects($this->never())
+            ->method('charge');
+
+        $sut = new OrderCharge(
+            $paymentApiFactory,
+            $configurationProvider,
+            $chargeRequestBuilder,
+        );
+
+        $sut->fullCharge($order);
+    }
+
+    public function testItShouldNotFullChargeIfPaymentAlreadyCharged(): void
+    {
+        $order = $this->createOrderEntity();
+        $transaction = new OrderTransactionEntity();
+        $transaction->setId('transaction_uuid');
+        $transaction->setCustomFields([
+            OrderTransactionDictionary::CUSTOM_FIELDS_NEXI_NETS_PAYMENT_ID => '025400006091b1ef6937598058c4e487',
+        ]);
+        $order->getTransactions()->add($transaction);
+
+        $configurationProvider = $this->createConfigurationProviderMock();
+        $configurationProvider
+            ->method('isAutoCharge')
+            ->with('test_sales_channel_id')
+            ->willReturn(false);
+
+        $paymentApi = $this->createMock(PaymentApi::class);
+        $paymentApi->expects($this->once())
+            ->method('retrievePayment')
+            ->with('025400006091b1ef6937598058c4e487')
+            ->willReturn($this->createAlreadyChargedRetrievePaymentResult());
+        $paymentApiFactory = $this->createPaymentApiFactoryMock($paymentApi);
+
+        $chargeRequestBuilder = $this->createMock(ChargeRequest::class);
+        $chargeRequestBuilder->expects($this->never())
+            ->method('buildFullCharge');
+
+        $paymentApi->expects($this->never())
+            ->method('charge');
+
+        $sut = new OrderCharge(
+            $paymentApiFactory,
+            $configurationProvider,
+            $chargeRequestBuilder,
+        );
+
+        $sut->fullCharge($order);
+    }
+
+    private function createOrderEntity(): OrderEntity
+    {
+        $order = new OrderEntity();
+        $order->setId('order_uuid');
+        $order->setSalesChannelId('test_sales_channel_id');
+        $order->setTransactions(new OrderTransactionCollection([]));
+
+        return $order;
+    }
+
+    private function createNotChargedRetrievePaymentResult(): RetrievePaymentResult
+    {
+        return RetrievePaymentResult::fromJson('{
+            "payment": {
+                "paymentId": "025400006091b1ef6937598058c4e487",
+                "summary": {
+                    "reservedAmount": 100
+                },
+                "consumer": {
+                    "shippingAddress": {},
+                    "billingAddress": {},
+                    "privatePerson": {},
+                    "company": {}
+                },
+                "paymentDetails": {},
+                "orderDetails": {
+                    "amount": 100,
+                    "currency": "EUR"
+                },
+                "checkout": {
+                    "url": "https://example.com/checkout",
+                    "cancelUrl": null
+                },
+                "created": "2019-08-24T14:15:22Z",
+                "refunds": [],
+                "charges": []
+            }
+        }');
+    }
+
+    private function createAlreadyChargedRetrievePaymentResult(): RetrievePaymentResult
+    {
+        return RetrievePaymentResult::fromJson('{
+            "payment": {
+                "paymentId": "025400006091b1ef6937598058c4e487",
+                "summary": {
+                    "reservedAmount": 100,
+                    "chargedAmount": 50
+                },
+                "consumer": {
+                    "shippingAddress": {},
+                    "billingAddress": {},
+                    "privatePerson": {},
+                    "company": {}
+                },
+                "paymentDetails": {},
+                "orderDetails": {
+                    "amount": 100,
+                    "currency": "EUR"
+                },
+                "checkout": {
+                    "url": "https://example.com/checkout",
+                    "cancelUrl": null
+                },
+                "created": "2019-08-24T14:15:22Z",
+                "refunds": [],
+                "charges": [
+                    {
+                        "chargeId": "test_charge_id",
+                        "amount": 50,
+                        "created": "2019-08-24T14:15:22Z",
+                        "orderItems": []
+                    }
+                ]
+            }
+        }');
+    }
+
+    private function createConfigurationProviderMock(): ConfigurationProvider|MockObject
+    {
+        return $this->createConfiguredMock(
+            ConfigurationProvider::class,
+            [
+                'getSecretKey' => 'secret',
+                'isLiveMode' => true,
+            ],
+        );
+    }
+
+    private function createPaymentApiFactoryMock(MockObject $paymentApi): PaymentApiFactory|MockObject
+    {
+        $paymentApiFactory = $this->createMock(PaymentApiFactory::class);
+        $paymentApiFactory
+            ->method('create')
+            ->with('secret', true)
+            ->willReturn($paymentApi);
+
+        return $paymentApiFactory;
+    }
+}
