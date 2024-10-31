7fb56286e9136ab592e387e7c25a63570cd2623b
l.brodowski <lukasz.brodowski@external.nexigroup.com>
2024-10-31 14:12:20 +0100

    feat: add charge method & refund modal

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,3 +1,4 @@
 import './extension/sw-flow-sequence-action';
 import "./services/nexinets-payment-detail.api.service";
+import "./services/nexinets-payment-actions.api.service";
 import "./sw-order-detail-details/";
