9f8bbb9e51655eb3e1e08e4b3fa56b186a03e345
Slawomir Rostek <slawomir.rostek@external.nexigroup.com>
2024-11-07 15:12:41 +0100

    update version to 1.5.7

--- src/NetsCheckout.php
+++ src/NetsCheckout.php
@@ -21,7 +21,7 @@ include_once __DIR__ . '/Compatibility/RouteScope.php';
 
 class NetsCheckout extends Plugin
 {
-    const PLUGIN_VERSION = '1.5.6';
+    const PLUGIN_VERSION = '1.5.7';
 
     public function install(InstallContext $context): void
     {
