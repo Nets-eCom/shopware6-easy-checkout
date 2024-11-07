9f9bc48cfbf11840b5fd9c254243badc14e20d9f
piotr.karszny <piotr.karszny@external.nexigroup.com>
2024-11-07 18:39:01 +0100

    refactor: use method for retrieval payment result test
    Refs: #98832, #100005

--- tests/CheckoutApi/Model/Result/RetrievePaymentResultTest.php
+++ tests/CheckoutApi/Model/Result/RetrievePaymentResultTest.php
@@ -9,40 +9,43 @@ use PHPUnit\Framework\TestCase;
 
 class RetrievePaymentResultTest extends TestCase
 {
-    private const PAYLOAD = <<<JSON
-    {
-        "payment": {
-            "paymentId": "025400006091b1ef6937598058c4e487",
-            "summary": {
-                "reservedAmount": 100
-            },
-            "consumer": {
-                "shippingAddress": {},
-                "billingAddress": {},
-                "privatePerson": {},
-                "company": {}
-            },
-            "paymentDetails": {},
-            "orderDetails": {
-                "amount": 100,
-                "currency": "EUR"
-            },
-            "checkout": {
-                "url": "https://example.com/checkout",
-                "cancelUrl": null
-            },
-            "created": "2019-08-24T14:15:22Z",
-            "refunds": [],
-            "charges": []
-        }
-    }
-JSON;
-
     public function testItCanInstantiateFromJsonString(): void
     {
         $this->assertInstanceOf(
             RetrievePaymentResult::class,
-            RetrievePaymentResult::fromJson(self::PAYLOAD)
+            RetrievePaymentResult::fromJson($this->getReservedPaymentResult())
         );
+    }
+
+    private function getReservedPaymentResult(): string
+    {
+        return <<<JSON
+        {
+            "payment": {
+                "paymentId": "025400006091b1ef6937598058c4e487",
+                "summary": {
+                    "reservedAmount": 100
+                },
+                "consumer": {
+                    "shippingAddress": {},
+                    "billingAddress": {},
+                    "privatePerson": {},
+                    "company": {}
+                },
+                "paymentDetails": {},
+                "orderDetails": {
+                    "amount": 100,
+                    "currency": "EUR"
+                },
+                "checkout": {
+                    "url": "https://example.com/checkout",
+                    "cancelUrl": null
+                },
+                "created": "2019-08-24T14:15:22Z",
+                "refunds": [],
+                "charges": []
+            }
+        }
+JSON;
     }
 }
