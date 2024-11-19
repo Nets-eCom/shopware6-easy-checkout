c3f8acb736071875e0bb07a7a7c3cf5095118b30
Slawomir Rostek <slawomir.rostek@external.nexigroup.com>
2024-11-19 13:21:50 +0100

    feat: #102587 added orderItems to nexinets-payment-detail response

--- src/Dictionary/OrderTransactionDictionary.php
+++ src/Dictionary/OrderTransactionDictionary.php
@@ -7,4 +7,6 @@ namespace NexiNets\Dictionary;
 class OrderTransactionDictionary
 {
     public const CUSTOM_FIELDS_NEXI_NETS_PAYMENT_ID = 'nexi_nets_payment_id';
+
+    public const CUSTOM_FIELDS_NEXI_NETS_ITEMS = 'nexi_nets_items';
 }
