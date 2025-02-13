e978dbafaa5c70fe228ec2984d913cb8febd344b
piotr.bandyk <piotr.bandyk@external.nexigroup.com>
2025-02-13 14:00:55 +0000

    Merged PR 44500: Release 13-02-2025

--- src/NetsCheckout.php
+++ src/NetsCheckout.php
@@ -21,7 +21,7 @@ include_once __DIR__ . '/Compatibility/RouteScope.php';
 
 class NetsCheckout extends Plugin
 {
-    const PLUGIN_VERSION = '1.5.7';
+    const PLUGIN_VERSION = '1.5.8';
 
     public function install(InstallContext $context): void
     {
