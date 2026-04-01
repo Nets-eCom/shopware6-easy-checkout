31ca25bb5124f220b5c01c68f4d2ac100166b1f5
Tomasz Czaplicki <tomasz.czaplicki@external.nexigroup.com>
2026-04-01 09:49:17 +0000

    Release 01-04-2026
    - style: change plugin icon
    - chore: update log file naming convention
    - feat: add test api connection on save button

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,4 +1,5 @@
 import './extension/sw-flow-sequence-action';
+import './extension/sw-system-config';
 import './services/nexi-checkout-payment-detail.api.service';
 import './services/nexi-checkout-payment-actions.api.service';
 import './services/nexi-checkout-credentials-test.api.service';
